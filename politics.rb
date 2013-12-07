a300fe5ed283910439c766d2a760ad7875131640
Mike Francis <mikeffrancis@gmail.com>
2013-12-07 12:12:06 +0000

    yay we got the title

--- politics.rb
+++ politics.rb
@@ -1,5 +1,14 @@
 require 'sinatra'
+require 'open-uri'
+require 'json'
+require 'pp'
 
 get '/' do
-  "Hello World"
+  
+  body = open('http://en.wikipedia.org/w/api.php?format=json&action=query&titles=Luxemburgism&prop=revisions&rvprop=content').read
+  data = JSON.parse(body)['query']['pages']
+  keys = data.keys
+  id = keys.first
+  pp(data[id]['title'])
+
 end
